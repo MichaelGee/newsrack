a8e8dbf308a85624de38666c03a64dd22c6aa232
Michael <michaelgatesjr@gmail.com>
2020-04-11 20:10:10 +0100

    navbar and card added for UI feel

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import "./App.css";
+import Navbar from "./components/navbar";
+import Cards from "./components/cards";
+import Banner from "./components/banner";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Navbar />
+      <Banner />
+      <Cards />
     </div>
   );
 }
---
+++ src/components/banner.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Banner = () => {
+  return (
+    <div className='my-6'>
+      <div className='text-center'>
+        <h1 className='text-4xl'>Top News</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
