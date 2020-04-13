20887494e46e2787e94e27ae3b017a5d4c9b608b
Michael <michaelgatesjr@gmail.com>
2020-04-13 12:13:22 +0100

    pushed an incomplete code with undefined images

--- src/App.js
+++ src/App.js
@@ -1,14 +1,87 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Navbar from "./components/navbar";
-import Cards from "./components/cards";
 import Banner from "./components/banner";
+import WorldCards from "./components/world/worldcards";
+import ScienceCards from "./components/science/sciencecards";
+import MoviesCards from "./components/movies/moviescards";
+import SportsCards from "./components/sports/sportscards";
+import axios from "axios";
 function App() {
+  /* World News */
+  const [worldImage, setWorldImage] = useState("");
+  const [worldTitle, setWorldTitle] = useState("");
+  const [worldAbstract, setWorldAbstract] = useState("");
+  const [worldByLine, setWorldByLine] = useState("");
+  const [worldTime, setWorldTime] = useState("");
+  const [worldUrl, setWorldUrl] = useState("");
+
+  /* Sports News */
+  const [sportImage, setSportImage] = useState("");
+  const [sportTitle, setSportTitle] = useState("");
+  const [sportAbstract, setSportAbstract] = useState("");
+  const [sportByLine, setSportByLine] = useState("");
+  const [sportTime, setSportTime] = useState("");
+  const [sportUrl, setSportUrl] = useState("");
+
+  /* Entertainment News */
+  const [movieImage, setMovieImage] = useState("");
+  const [movieTitle, setMovieTitle] = useState("");
+  const [movieAbstract, setMovieAbstract] = useState("");
+  const [movieByLine, setMovieByLine] = useState("");
+  const [movieTime, setMovieTime] = useState("");
+  const [movieUrl, setMovieUrl] = useState("");
+
+  /* Science News */
+  const [scienceImage, setScienceImage] = useState("");
+  const [scienceTitle, setScienceTitle] = useState("");
+  const [scienceAbstract, setScienceAbstract] = useState("");
+  const [scienceByLine, setScienceByLine] = useState("");
+  const [scienceTime, setScienceTime] = useState("");
+  const [scienceUrl, setScienceUrl] = useState("");
+
+  /* World */
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.nytimes.com/svc/topstories/v2/world.json?api-key=${process.env.REACT_APP_API_KEY}`
+      )
+      .then((res) => {
+        /*   console.log(res.data.results[0]); */
+        const dataData = res.data.results[0];
+        const dataFromAPI = res.data.results;
+        const newsArray = dataFromAPI.map((news) => news);
+        const items = newsArray.slice(0, 10);
+        console.log("i: ", items);
+        /* const imageUrl = dataData.multimedia[items].url; */
+        const imageUrl = dataFromAPI.map((media) => media.multimedia);
+        const allImage = imageUrl.map((img) => img.url);
+        console.log("img: ", allImage);
+        setWorldImage(imageUrl);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div className='App'>
       <Navbar />
       <Banner />
-      <Cards />
+      <h1>World News</h1>
+      <div className='word-wrapper'>
+        <WorldCards />
+      </div>
+      <h1>Sports</h1>
+      <div className='science-wrapper'>
+        <ScienceCards />
+      </div>
+      <h1>Entertainment</h1>
+      <div className='movie-wrapper'>
+        <MoviesCards />
+      </div>
+      <h1>Science</h1>
+      <div className='sports-wrapper'>
+        <SportsCards />
+      </div>
     </div>
   );
 }
--- src/components/movies/moviescards.js
+++ src/components/movies/moviescards.js
@@ -1,15 +1,12 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React from "react";
 
-const Cards = () => {
-  useEffect(() => {}, []);
-
+const MoviesCards = () => {
   return (
     <div>
       <div className='shadow-lg rounded-lg bg-white mx-auto m-8 p-4 notification-box w-3/12'>
         {/* <div className="text-sm pb-2">{image}</div> */}
         <div className='text-sm pb-2'>
-          {/*  <img src={image} alt="news-article" /> */}
+          {/* <img src={image} alt='news-article' /> */}
         </div>
         <div className='text-sm text-gray-600  tracking-tight '>
           I will never close automatically. This is a purposely very very long
@@ -20,4 +17,4 @@ const Cards = () => {
   );
 };
 
-export default Cards;
+export default MoviesCards;
