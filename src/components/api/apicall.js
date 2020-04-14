72ba41630b65dcb8827d9caaa49cd17d40b92636
Benedict Nkeonye <benedictiknkeonye@gmail.com>
2020-04-14 14:39:54 +0100

    Feat: data access and distribution completed

--- src/components/api/apicall.js
+++ src/components/api/apicall.js
@@ -18,3 +18,20 @@ export const fetchData = async (newsType) => {
     console.log(error);
   }
 };
+
+export const newsArray = (articleType) => {
+  return articleType.results.slice(0, 10).map((article) => {
+    return {
+      img: article.multimedia[0].url,
+      imgCaption:
+        article.multimedia[0].caption !== ''
+          ? article.multimedia[0].caption
+          : 'N/A',
+      title: article.title,
+      abstract: article.abstract,
+      byline: article.byline,
+      release_date: article.created_date,
+      url: article.short_url,
+    };
+  });
+};
