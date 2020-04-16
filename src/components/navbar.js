7d43bb1cbf9ffb8166f493f0e02a929d4afd35e7
Michael <michaelgatesjr@gmail.com>
2020-04-16 15:12:04 +0100

    added logo

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Logo from "../assets/newspaper.png";
 
 const Navbar = () => {
   return (
@@ -15,7 +16,13 @@ const Navbar = () => {
                 <path d='M16.4 9H3.6c-.552 0-.6.447-.6 1 0 .553.048 1 .6 1h12.8c.552 0 .6-.447.6-1 0-.553-.048-1-.6-1zm0 4H3.6c-.552 0-.6.447-.6 1 0 .553.048 1 .6 1h12.8c.552 0 .6-.447.6-1 0-.553-.048-1-.6-1zM3.6 7h12.8c.552 0 .6-.447.6-1 0-.553-.048-1-.6-1H3.6c-.552 0-.6.447-.6 1 0 .553.048 1 .6 1z' />
               </svg> */}
             </div>
-            <div className='logo w-1/2 md:w-auto text-center text-black text-2xl font-medium'>
+
+            <div className='flex w-1/2 md:w-auto text-center text-black text-2xl font-medium'>
+              <img
+                src={Logo}
+                alt='newpaper_logo'
+                className='w-8 sm:w-10 mr-2'
+              />
               NewSRacK
             </div>
           </div>
