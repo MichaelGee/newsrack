1648f522496d1f9d42b7e70bb63afbeafb78e6be
Michael <michaelgatesjr@gmail.com>
2020-06-08 09:37:10 +0100

    greetings added

--- src/components/banner.js
+++ src/components/banner.js
@@ -1,10 +1,19 @@
 import React from "react";
 
 const Banner = () => {
+  let today = new Date();
+  let curHr = today.getHours();
   return (
     <div className='my-6'>
       <div className='text-center'>
-        <h1 className='text-4xl'>Top News</h1>
+        <h1 className='md:text-6xl lg:text-4xl font-medium'>
+          {curHr < 12
+            ? "Good Morning"
+            : curHr < 18
+            ? "Good Afternoon"
+            : "Good Evening"}
+        </h1>
+        <h1 className='md:text-4xl lg:text-1xl'>Top News</h1>
       </div>
     </div>
   );
